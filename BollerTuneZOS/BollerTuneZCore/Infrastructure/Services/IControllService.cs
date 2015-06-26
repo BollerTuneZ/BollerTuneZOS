32fc4a2521ffe64fb89e9f01e41dfa566ff58001
jonas@juanah.de <jonas@juanah.de>
2015-06-26 23:30:47 +0200

    BollerTuneZ logik in einen service verfrachtet

---
+++ BollerTuneZOS/BollerTuneZCore/Infrastructure/Services/IControllService.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Services
+{
+    /// <summary>
+    /// Represents Controlling Process Service
+    /// Jonas Ahlf 26.06.2015 23:04:05
+    /// </summary>
+    public interface IControllService
+    {
+        /// <summary>
+        /// Starts the Service
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stops the Service
+        /// </summary>
+        void Stop();
+    }
+}
