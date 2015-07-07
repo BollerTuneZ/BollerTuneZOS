80cfca502137099c5c35da76082793b723aec6f4
steffenreimann <steffenreimann@steffens-mbp.fritz.box>
2015-07-07 23:29:32 +0200

    Alle Config Sachen wie Motor und Lenkung erweitert das alle immer die gleichen werte haben auch wenn man neu verbunden wird  bekommt man die richtigen Daten! Steuerung wurden w a s d tasten eingebaut und mit js wird das css der buttons geändert .

--- node.btz-1.2.2/public/controll.js
+++ node.btz-1.2.2/public/controll.js
@@ -1,14 +1,69 @@
 $(document).ready(function(){
 
-	$('#searchbox input').bind('keypress', function(e) {
-	if(e.keyCode==13){
-		// Enter pressed... do anything here...
-	}
-	});
+	var itemw = document.getElementById("w");
+	var itema = document.getElementById("a");
+	var items = document.getElementById("s");
+	var itemd = document.getElementById("d");
 	
-	$(document).keydown(function(event){ 
-    $("LenkmotorSpeedMax").html("Key: " + event.which);
-});
+
+	document.body.onkeydown = function(e){
+    //String.fromCharCode(w.keyCode)
+    	if (e.keyCode==87) {
+    	
+    	
+    		itemw.setAttribute("style", "background-color:gray; width: 100px ; height: 100px")
+			console.log("Press W") 
+		}
+		
+		if (e.keyCode==65) {
+			itema.setAttribute("style", "background-color:gray; width: 100px ; height: 100px")
+	    	console.log("Press A") 
+		}
+		
+		if (e.keyCode==83) {
+			items.setAttribute("style", "background-color:gray; width: 100px ; height: 100px")
+	    	console.log("Press S") 
+		}
+		
+		if (e.keyCode==68) {
+			itemd.setAttribute("style", "background-color:gray; width: 100px ; height: 100px")
+	    	console.log("Press D") 
+		}
+		
+		
+	};
+
+	document.body.onkeyup = function(r){
+    //String.fromCharCode(w.keyCode)
+    	if (r.keyCode==87) {
+    		itemw.setAttribute("style", " width: 100px ; height: 100px")
+			console.log("keyup W") 
+		
+		}
+		
+		if (r.keyCode==65) {
+			itema.setAttribute("style", " width: 100px ; height: 100px")
+	    	console.log("keyup A") 
+		}
+		
+		if (r.keyCode==83) {
+			items.setAttribute("style", " width: 100px ; height: 100px")
+	    	console.log("keyup S") 
+		}
+		
+		if (r.keyCode==68) {
+			itemd.setAttribute("style", " width: 100px ; height: 100px")
+	    	console.log("keyup D") 
+		}
+		
+		
+	};
 	
+
+
+
+
+
+
 
 });
